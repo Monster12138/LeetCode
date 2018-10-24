bc1fd076e22d0e0b20a046fd6c3d6faccc37d871
Monster12138 <838009062@qq.com>
2018-10-24 14:35:22 +0800

    7.1

--- MostWater.cpp
+++ MostWater.cpp
@@ -5,14 +5,23 @@ using namespace std;
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-         int max_are = 0;
-         for(unsigned int i = 0; i < height.size(); i++){
-             for(unsigned int j = i+1; j < height.size(); j++){
-                 int are = (j-i) * (height[i]<height[j]?height[i]:height[j]);
-                 if(max_are < are)max_are = are;
-             }
-         }
-         return max_are;
+        /* 暴力法
+           int max_are = 0;
+           for(unsigned int i = 0; i < height.size(); i++){
+           for(unsigned int j = i+1; j < height.size(); j++){
+           int are = (j-i) * (height[i]<height[j]?height[i]:height[j]);
+           if(max_are < are)max_are = are;
+           }
+           }
+           return max_are;
+           */
+
+        int max_are = 0, i = 0, j = height.size() - 1;
+        while(i<j){
+            max_are = max(max_are,((j-i) * (height[i] < height[j]?height[i]:height[j])));
+            height[i]<height[j]?++i:--j;
+        }
+        return max_are;
     }
 };
 
