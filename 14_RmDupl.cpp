fa4a6a620f75531317bb845a018703928eb05e4a
Monster12138 <838009062@qq.com>
2018-11-01 15:05:50 +0800

    14+

--- 14_RmDupl.cpp
+++ 14_RmDupl.cpp
@@ -5,6 +5,7 @@ using namespace std;
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+        /*     1.0
         int  i = nums.size() - 1,length = nums.size(), end = 0;
         while(i>0){
             if(nums[i] == nums[i-1])end = i;
@@ -21,6 +22,18 @@ public:
             length -= (end - i);
         }
         return length;
+        */
+/*2.0
+        if(nums.empty())return 0;
+        unsigned int i = 0, j = 1;
+        while(j < nums.size()){
+            if(nums[j] != nums[j-1]){
+                nums[++i] = nums[j];
+            }
+            ++j;
+        }
+        return i+1;
+*/
     }
 };
 
