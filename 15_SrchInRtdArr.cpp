d1ba1b606b732f3b70359e90ea12d159c26f921c
Monster12138 <838009062@qq.com>
2018-11-02 16:51:36 +0800

    15

---
+++ 15_SrchInRtdArr.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        /* 1.0
+        unsigned int i = 0;
+        for(; i < nums.size(); ++i){
+            if(target == nums[i])return i;
+            if( i>0 && target < nums[i-1] && target > nums[i])return -1;
+        }
+        return -1;
+        */
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
+
