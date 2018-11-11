5326468329a8e0e98592bde5069a66a763677ca5
Monster12138 <838009062@qq.com>
2018-11-11 23:49:09 +0800

    22

---
+++ 22_Unique_Paths.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        if(m == 0|| n == 0)return 0;
+        int arr[m][n];
+        for(int i = 0; i < m; ++i){
+            for(int j = 0; j < n; ++j){
+                if(i == 0 || j ==0)arr[i][j] = 1;
+                else {
+                    arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
+                }
+            }
+        }
+        return arr[m-1][n-1];
+    }
+};
+
+int main()
+{
+    int m = 7, n = 0;
+    Solution s;
+    cout << s.uniquePaths(m, n) << endl;
+    return 0;
+}
+
