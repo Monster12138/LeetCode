fe9638cc3cf00df7117dcea9c98d10ff903f1aa6
Monster12138 <838009062@qq.com>
2018-11-20 01:16:18 +0800

    29

--- 29_Best_time2.cpp
+++ 29_Best_time2.cpp
@@ -1,4 +1,19 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int res = 0;
+        for(int i = 0; i < (int)prices.size() - 1; ++i){
+            if(prices[i] < prices[i+1]){
+                res += prices[i+1] - prices[i];
+            }
+        }
+        return res;
+    }
+};
 
 int main()
 {
