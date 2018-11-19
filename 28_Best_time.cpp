34f8228297f5ef1abec03210f574031006d8efb1
Monster12138 <838009062@qq.com>
2018-11-19 23:44:29 +0800

    29

--- 28_Best_time.cpp
+++ 28_Best_time.cpp
@@ -1,19 +1,37 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int max = 0;
+        /*
+        int res =  0;
         for(int i = 0; i < (int)prices.size(); ++i){
-            for(int j = i + 1; j < (int)prices.size(); ++j){
-                if(prices[j] < prices[i])continue;
-                if(prices[j] - prices[i] > max)max = prices[j] - prices[i];
-            }
+            vector<int> tmp(prices.begin() + i, prices.end());
+            sort(tmp.begin(), tmp.end());
+            int max = tmp.back() - prices[i];
+            if(max > res)res = max;
         }
 
-        return max;
+        return res;
+        */
+
+        int size = prices.size();
+        vector<int> db(size + 1, 0);
+
+        int max_p = prices.back();
+        for(int i = size - 1; i >=0; --i){
+            if(prices[i] > max_p){
+                max_p = prices[i];
+                db[i] = db[i+1];
+            }
+            else{
+                db[i] = max(max_p - prices[i], db[i+1]);
+            }
+        }
+        return db[0];
     }
 };
 
