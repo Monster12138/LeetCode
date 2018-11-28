03b90bf4cd02de307a10dd6e77552e7827ba1ca6
Monster12138 <838009062@qq.com>
2018-11-29 01:51:47 +0800

    38

--- 38_.cpp
+++ 38_.cpp
@@ -1,4 +1,38 @@
 #include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == nullptr || head->next == nullptr)return head;
+        /*
+        ListNode *cur = head, *_next = head->next;
+        
+        cur->next = nullptr;
+        while(_next){
+            ListNode *tmp = _next->next;
+            _next->next = cur;
+            cur = _next;
+            _next = tmp;
+        }
+        
+        return cur;
+        */
+        
+        ListNode *p = head;
+        head = reverseList(p->next);
+        p->next->next = p;
+        p->next = nullptr;
+        
+        return head;
+    }
+};
 
 int main()
 {
