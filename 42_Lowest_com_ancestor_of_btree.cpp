97cf17933f83b73b577aeba52436169d665064ad
Monster12138 <838009062@qq.com>
2018-12-05 02:04:34 +0800

    42

--- 42_Lowest_com_ancestor_of_btree.cpp
+++ 42_Lowest_com_ancestor_of_btree.cpp
@@ -8,10 +8,24 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
+static const auto _=[]{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
 class Solution {
 public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {  
+        TreeNode *cur = root;
+        while(1){
+            if(cur==nullptr) return nullptr;
+            if(p==cur || q==cur) return cur;
+            else if(cur->val < p->val && cur->val < q->val)
+                cur = cur->right;
+            else if(cur->val > p->val && cur->val > q->val)
+                cur = cur->left;
+            else return cur;
+        }
     }
 };
 
